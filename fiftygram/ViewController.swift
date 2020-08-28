ce532566f72a5748a01e64148514f41717f5cdac
Sanket <mrockdud28@gmail.com>
2020-08-28 19:10:15 +0530

    Initial Commit

---
+++ fiftygram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fiftygram
+//
+//  Created by Sanket singla on 28/08/20.
+//  Copyright © 2020 Sanket singla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
